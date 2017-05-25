d90c790e079dca7eb0d0ffd1fac016fd5b919eb0
Alexey Fominykh <alexey.fominykh@namecheap.com>
2017-05-25 15:03:23 +0300

    fix

--- server/models/todo.js
+++ server/models/todo.js
@@ -1,20 +1,20 @@
-let mongoose = require('mongoose');
+var mongoose = require('mongoose');
 
-let Todo = mongoose.model('Todo', {
-    text: {
-        type: String,
-        required: true,
-        minlength: 1,
-        trim: true
-    },
-    completed: {
-        type: Boolean,
-        default: false
-    },
-    completedAt: {
-        type: Number,
-        default: null
-    }
+var Todo = mongoose.model('Todo', {
+  text: {
+    type: String,
+    required: true,
+    minlength: 1,
+    trim: true
+  },
+  completed: {
+    type: Boolean,
+    default: false
+  },
+  completedAt: {
+    type: Number,
+    default: null
+  }
 });
 
 module.exports = {Todo};
